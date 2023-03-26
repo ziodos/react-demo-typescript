4b12fec43507ed7937a8ce1c6c58086376a76365
ziodos <zied.zanin14@gmail.com>
2023-03-26 20:15:47 +0200

    feat: add album initial feature with search

---
+++ src/features/products/AlbumList.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import useDebounce from "../../hooks/useDebounce";
+import AlbumSearch from "./AlbumSearch";
+
+const AlbumList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    return (
+        <>
+            <h1>Debounce example</h1>
+            <p>Start typing some book name. Search starts at length 5</p>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Search books"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
+            <AlbumSearch searchTerm={debouncedSearchTerm} />
+        </>
+    );
+};
+
+export default AlbumList;
---
+++ src/features/products/AlbumSearch.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, FC } from "react";
+import { useSearchProductsQuery } from "./albumsSlice";
+
+type BookSearchResultsProps = {
+    searchTerm: string;
+};
+
+const AlbumSearch: FC<BookSearchResultsProps> = ({
+    searchTerm,
+}: BookSearchResultsProps) => {
+    const [filteredSearchTerm, setFilteredSearchTerm] = useState(searchTerm);
+    const { data, error, isLoading, isFetching } =
+        useSearchProductsQuery(filteredSearchTerm);
+    const books = data ?? [];
+
+    useEffect(() => {
+        setFilteredSearchTerm(searchTerm);
+    }, [searchTerm]);
+
+    if (error) {
+        return <div className="text-hint">Error while fetching books</div>;
+    }
+
+    if (isLoading) {
+        return <div className="text-hint">Loading books...</div>;
+    }
+
+    if (isFetching) {
+        return <div className="text-hint">Fetching books...</div>;
+    }
+
+    if (books.length === 0) {
+        return <div className="text-hint">No books found</div>;
+    }
+
+    return (
+        <ul>
+            {books.map(({ id, brand, category, description }) => (
+                <li key={id}>
+                    {brand}: {category}, {description}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default AlbumSearch;
